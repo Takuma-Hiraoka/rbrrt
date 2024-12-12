1fa95250eeec5d56f8251c0beece7e2757275bf4
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-12-12 20:35:34 +0900

    fix reset-manip-pose

--- rbrrt_sample/src/jaxon_common.cpp
+++ rbrrt_sample/src/jaxon_common.cpp
@@ -12,13 +12,13 @@ namespace rbrrt_sample {
     cnoid::BodyLoader bodyLoader;
     param->robot = bodyLoader.load(ros::package::getPath("jvrc_models") + "/JAXON_JVRC/JAXON_JVRCmain.wrl");
     if(!(param->robot)) std::cerr << "!robot" << std::endl;
-    param->robot->rootLink()->p() = cnoid::Vector3(0,0,1.0);
+    param->robot->rootLink()->p() = cnoid::Vector3(0,0,0.88);
     param->robot->rootLink()->v().setZero();
     param->robot->rootLink()->R() = cnoid::Matrix3::Identity();
     param->robot->rootLink()->w().setZero();
     std::vector<double> reset_manip_pose{
-      0.0, 0.0, -0.349066, 0.698132, -0.349066, 0.0,// rleg
-        0.0, 0.0, -0.349066, 0.698132, -0.349066, 0.0,// lleg
+      0.0, 0.0, -0.549066, 1.298132, -0.749066, 0.0,// rleg
+        0.0, 0.0, -0.549066, 1.298132, -0.749066, 0.0,// lleg
         0.0, 0.0, 0.0, // torso
         0.0, 0.0, // head
         0.0, 0.959931, -0.349066, -0.261799, -1.74533, -0.436332, 0.0, -0.785398,// rarm
